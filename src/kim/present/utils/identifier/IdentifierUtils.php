972eafd4bf6f02901304dfa1079df6e9ab28f199
PresentKim <debe3721@gmail.com>
2022-01-15 17:21:22 +0900

    refactor : Code cleanup improves readability of registerItem()

--- src/kim/present/utils/identifier/IdentifierUtils.php
+++ src/kim/present/utils/identifier/IdentifierUtils.php
@@ -23,6 +23,8 @@
  *
  * @noinspection PhpUnused
  * @noinspection SpellCheckingInspection
+ * @noinspection PhpDeprecationInspection
+ * @noinspection PhpUndefinedFieldInspection
  */
 
 declare(strict_types=1);
@@ -37,57 +39,67 @@ use pocketmine\network\mcpe\convert\ItemTranslator;
 use pocketmine\network\mcpe\protocol\serializer\ItemTypeDictionary;
 use pocketmine\network\mcpe\protocol\types\CacheableNbt;
 use pocketmine\network\mcpe\protocol\types\ItemTypeEntry;
+use RuntimeException;
 
 final class IdentifierUtils{
-    /** @internal */
-    public static int $runtimeIdCursor = 0xfff;
+    private static int $itemRuntimeId = 1;
 
     private function __construct(){ }
 
-    /** @noinspection PhpUndefinedFieldInspection */
     public static function registerItem(string $stringId, int $legacyId, int $legacyMeta = -1) : void{
-        (function() use ($stringId, $legacyId, $legacyMeta){ //HACK : Closure bind hack to access inaccessible members
-            $runtimeId = null;
+        /**
+         * Gets the item Runtime ID to use for the item currently being added
+         * Based on not registered in ItemTranslator
+         *
+         * @see ItemTranslator::simpleNetToCoreMapping
+         * @see ItemTranslator::complexNetToCoreMapping
+         */
+        $runtimeId = null;
+        (static function() use (&$runtimeId){
+            $itemTranslator = ItemTranslator::getInstance();
             while($runtimeId === null){
-                if(IdentifierUtils::$runtimeIdCursor >= 0xffff){
+                if(self::$itemRuntimeId >= 0xffff){
                     throw new OverflowException("There are no more item runtime ids available.");
                 }
-                $cursor = IdentifierUtils::$runtimeIdCursor++;
-                /**
-                 * @see ItemTranslator::simpleNetToCoreMapping
-                 * @see ItemTranslator::complexNetToCoreMapping
-                 */
-                if(!isset($this->simpleNetToCoreMapping[$cursor]) && !isset($this->complexNetToCoreMapping[$cursor])){
-                    $runtimeId = $cursor;
-                }
+                $cursor = self::$itemRuntimeId++;
+                $runtimeId = (function() use ($cursor){ //HACK : Closure bind hack to access inaccessible members
+                    return !isset($this->simpleNetToCoreMapping[$cursor]) && !isset($this->complexNetToCoreMapping[$cursor]) ? $cursor : null;
+                })->call($itemTranslator);
             }
+        })();
+
+        /**
+         * Cross-Mapping Runtime ID and Legacy ID to ItemTranslator
+         *
+         * @see ItemTranslator::simpleCoreToNetMapping
+         * @see ItemTranslator::simpleNetToCoreMapping
+         * @see ItemTranslator::complexCoreToNetMapping
+         * @see ItemTranslator::complexNetToCoreMapping
+         */
+        (function() use ($runtimeId, $legacyId, $legacyMeta){ //HACK : Closure bind hack to access inaccessible members
             if($legacyMeta === -1){
                 // simple mapping - When the same item has multiple meta. ex) IronHoe
-                /** @see ItemTranslator::simpleCoreToNetMapping */
                 $this->simpleCoreToNetMapping[$legacyId] = $runtimeId;
-
-                /** @see ItemTranslator::simpleNetToCoreMapping */
                 $this->simpleNetToCoreMapping[$runtimeId] = $legacyId;
             }else{
                 // complex mapping - When items are classified according to meta. ex) Bucket
-                /** @see ItemTranslator::complexCoreToNetMapping */
                 $this->complexCoreToNetMapping[$legacyId][$legacyMeta] = $runtimeId;
-
-                /** @see ItemTranslator::complexNetToCoreMapping */
                 $this->complexNetToCoreMapping[$runtimeId] = [$legacyId, $legacyMeta];
             }
-
-            (function() use ($stringId, $runtimeId){ //HACK : Closure bind hack to access inaccessible members
-                /** @see ItemTypeDictionary::itemTypes */
-                $this->itemTypes[] = new ItemTypeEntry($stringId, $runtimeId, true);
-
-                /** @see ItemTypeDictionary::intToStringIdMap */
-                $this->stringToIntMap[$stringId] = $runtimeId;
-
-                /** @see ItemTypeDictionary::stringToIntMap */
-                $this->intToStringIdMap[$runtimeId] = $stringId;
-            })->call(GlobalItemTypeDictionary::getInstance()->getDictionary());
         })->call(ItemTranslator::getInstance());
+
+        /**
+         * Cross-Mapping Runtime ID and String ID to ItemTypeDictionary
+         *
+         * @see ItemTypeDictionary::itemTypes
+         * @see ItemTypeDictionary::stringToIntMap
+         * @see ItemTypeDictionary::intToStringIdMap
+         */
+        (function() use ($stringId, $runtimeId){ //HACK : Closure bind hack to access inaccessible members
+            $this->itemTypes[] = new ItemTypeEntry($stringId, $runtimeId, true);
+            $this->stringToIntMap[$stringId] = $runtimeId;
+            $this->intToStringIdMap[$runtimeId] = $stringId;
+        })->call(GlobalItemTypeDictionary::getInstance()->getDictionary());
     }
 
     public static function registerEntity(string $entityIdentifier) : void{
